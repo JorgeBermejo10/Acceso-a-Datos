4aeefc2d8f221885e9d3d5d58711b6ffa90a6666
Jorge <jorgebermejo>
2023-11-06 13:41:22 +0100

    Se añade ejercicio 7 al repositorio

---
+++ ejercicio8.py
@@ -0,0 +1,7 @@
+asignaturas = ["iOS","PSP", "Acceso a datos", "Diseño de interfaces", "Inglés"]
+
+def lista(asignaturas) :
+    for asig in asignaturas :
+        print(asig)
+        
+lista (asignaturas)
